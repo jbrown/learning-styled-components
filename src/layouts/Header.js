31624b6962428d6294e123fe3dc72f7e10c5d4c1
Justin Brown <justinbrown.work@gmail.com>
2019-01-27 19:31:59 -0600

    Cleanup header component structure.

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,21 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import logo from '../logo.svg'
 
-const Header = () => {
-  return (
-    <AppHeader>
-      <img src={logo} alt="logo" className="logo" />
-    </AppHeader>
-  )
-}
+const Header = ({ className }) => (
+  <header className={className}>
+    <img src={logo} alt="logo" className="logo" />
+  </header>
+)
 
-const AppHeader = styled.header`
+export default styled(Header)`
   background: #524763;
   padding: 10px 5%;
-  
   .logo {
     width: 40px;
   }
 `
-
-export default Header
