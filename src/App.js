ae940598905175c4d48a2c74ee3c419e7734f098
Justin Brown <justinbrown.work@gmail.com>
2019-01-27 19:11:39 -0600

    Cleanup App and create a Header component.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
 import React, { Component } from 'react';
-import styled, { css } from 'styled-components';
-import logo from './logo.svg';
 import GlobalStyle from './Global';
-import { Button, CancelButton, Heading } from './elements';
+import Header from './layouts/Header';
 
-const Fake = ({ className }) => (
-  <div className={className}>
-    <h2>I'm a fake component</h2>
-  </div>
-);
-
-const AppWrapper = styled.div`
-  header {
-    background: teal;
-  }
-  ${Button} {
-    margin-bottom: 2rem;
-  }
-`
-
-const DoubleFake = styled(Fake)`
-  h2 {
-    color: red;
-  }
-`
 class App extends Component {
   render() {
     return (
-      <AppWrapper>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Heading>
-            Edit <code>src/App.js</code> and save to reload.
-          </Heading>
-          <DoubleFake />
-          <Fake />
-          <Button>Save</Button>
-          <CancelButton top="0">Cancel</CancelButton>
-          <Heading>Heading two</Heading>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
         <GlobalStyle />
-      </AppWrapper>
+      </div>
     );
   }
 }
